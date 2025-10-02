f422f6070921cac66f7e009122ad56d5933f1965
mdspain-dbre <michael.dspain@vizio.com>
2025-10-02 13:33:12 -0600

    add main file

---
+++ main.tf
@@ -0,0 +1,12 @@
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
+resource "google_sql_database_instance" "md-mysql-db-instance" {
+  name             = "md-mysql-db-instance"
+  region           = "us-central1"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = false
+}
+
