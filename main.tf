bd669e63bfdd3267425232b044ff341b0f48cf1e
mdspain-dbre <michael.dspain@vizio.com>
2025-10-02 15:33:24 -0600

    new bucket

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "google_sql_database_instance" "md-mysql-db-instance-1" {
     tier = "db-f1-micro"
   }
 
-  deletion_protection  = false
+  deletion_protection = false
 }
 
 resource "google_sql_database" "md-mysql-db-tf" {
@@ -31,6 +31,14 @@ resource "google_storage_bucket" "mdtestbucket-gcp-terraform" {
 
 resource "google_storage_bucket" "mdtestbucket-gcp-terraform2" {
   name          = "mdtestbucket-gcp-terraform2"
+  location      = "us-central1"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
+
+resource "google_storage_bucket" "mdtestbucket-gcp-terraform3" {
+  name          = "mdtestbucket-gcp-terraform3"
   location      = "us-central1"
   force_destroy = true
 
