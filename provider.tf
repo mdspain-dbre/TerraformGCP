8247e04e50de60ee9adea23a4bd094235f43f772
mdspain-dbre <michael.dspain@vizio.com>
2025-10-02 13:55:10 -0600

    file shit

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   project     = "dre-sandbox-471618"
   region      = "us-central1"
-  credentials = "/Users/michael.dspain/Documents/SSH_Key/YomommKey.json"
+  credentials = "YomommKey.json"
 }
