954f8760b61e05e62f0aadfe6ed764f7b9b3dbf3
mdspain-dbre <michael.dspain@vizio.com>
2025-10-02 14:46:25 -0600

    bucket

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
-resource "google_sql_database_instance" "md-mysql-db-instance" {
-  name             = "md-mysql-db-instance"
+resource "google_sql_database_instance" "md-mysql-db-instance-1" {
+  name             = "md-mysql-db-instance-1"
   region           = "us-central1"
   database_version = "MYSQL_8_0"
   settings {
@@ -12,11 +12,19 @@ resource "google_sql_database_instance" "md-mysql-db-instance" {
 
 resource "google_sql_database" "md-mysql-db-tf" {
   name     = "md-test-db"
-  instance = google_sql_database_instance.md-mysql-db-instance.name
+  instance = google_sql_database_instance.md-mysql-db-instance-1.name
 }
 
 resource "google_sql_user" "myuser" {
   name     = "md-testuser"
-  instance = google_sql_database_instance.md-mysql-db-instance.name
+  instance = google_sql_database_instance.md-mysql-db-instance-1.name
   password = "testuser123"
+}
+
+resource "google_storage_bucket" "mdtestbucket" {
+  name          = "mdtestbucket"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
   project     = "dre-sandbox-471618"
   region      = "us-central1"
-  credentials = "~/Users/michael.dspain/Documents/SSH_Key/YomommKey.json"
+  credentials = "/Users/michael.dspain/Documents/SSH_Key/dre-sandbox-471618-d1b8b7ca9d50.json"
 }
